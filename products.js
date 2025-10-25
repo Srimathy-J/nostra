b5038fa76e2fe471df4c108177006f4548606e0d
Srimathy-J <srimathyjayaseelan@gmail.com>
2025-10-25 14:12:19 +0530

    updates

--- products.js
+++ products.js
@@ -1,80 +1,130 @@
 const products = [
     {
         id:1,
-        name:"Floral Summer Shirt",
+        name:"Summer Casual Shirt",
         src:"products/f1.jpg",
         desc:"",
-        price:220,
-        tags:["new","blue","summer"]
+        price:290,
+        tags:["new","blue","summer","Men"]
     },
-
     {
         id:2,
-        name:"Summer Green",
-        src:"products/f2.jpg",
+        name:"Blue Formal Shirt",
+        src:"products/n1.jpg",
         desc:"",
-        price:260,
-        tags:["new","green","beach"]
+        price:590,
+        tags:["new","blue","summer","Men"]
     },
+
 
     {
         id:3,
+        name:"floral Green Casual shirt",
+        src:"products/f2.jpg",
+        desc:"",
+        price:260,
+        tags:["new","green","beach,casual","Men"]
+    },
+    {
+        id:4,
+        name:"Floral Palazzo",
+        src:"products/f7.jpg",
+        desc:"",
+        price:1290,
+        tags:["new","summer,casuals","Women"]
+    },
+
+
+    {
+        id:5,
         name:"Party Floral Shirt",
         src:"products/f3.jpg",
         desc:"",
-        price:399,
-        tags:["old","red","party"]
+        price:499,
+        tags:["old","red","party","Men"]
     },
 
     {
-        id:4,
+        id:6,
         name:"Floral Summer Shirt",
         src:"products/f4.jpg",
         desc:"",
         price:399,
-        tags:["old","white","beach"]
+        tags:["old","white","beach","Men"]
     },
     {
-        id:5,
-        name:"Beach Shirt",
+        id:7,
+        name:"Beach vibe Shirt",
         src:"products/f5.jpg",
         desc:"",
-        price:579,
-        tags:["old","white","beach"]
+        price:599,
+        tags:["old","beach","Men"]
     }
 ,
     {
-        id:6,
-        name:"Shirt Party Red",
+        id:8,
+        name:"Half & Half shirt",
         src:"products/f6.jpg",
         desc:"",
-        price:579,
-        tags:["old","red","party"]
+        price:899,
+        tags:["old","red","party","Men"]
     },
 
     {
-        id:7,
-        name:"Party Floral Shirt",
-        src:"products/f3.jpg",
+        id:9,
+        name:"Gray Formal Shirt",
+        src:"products/n2.jpg",
         desc:"",
         price:399,
-        tags:["old","red","party"]
+        tags:["old","white","party","Men"]
     },
     {
-        id:8,
-        name:"Shirt Party Red",
-        src:"products/f6.jpg",
+        id:10,
+        name:"White Formal wear",
+        src:"products/n3.jpg",
         desc:"",
         price:579,
-        tags:["old","red","party"]
+        tags:["old","white","party","Men"]
     },
     {
-        id:9,
+        id:11,
         name:"Floral Summer Shirt",
-        src:"products/f1.jpg",
+        src:"products/n4.jpg",
         desc:"",
         price:220,
-        tags:["new","blue","summer"]
+        tags:["new","summer"]
+    },
+    {
+        id:12,
+        name:"Blue Denim Shirt",
+        src:"products/n5.jpg",
+        desc:"",
+        price:220,
+        tags:["new","blue"]
+    },
+    {
+        id:13,
+        name:"Casual Shirt",
+        src:"products/n7.jpg",
+        desc:"",
+        price:220,
+        tags:["new"]
+    },
+    {
+        id:14,
+        name:"Black formal Shirt",
+        src:"products/n8.jpg",
+        desc:"",
+        price:1390,
+        tags:["new","summer"]
+    },
+    {
+        id:15,
+        name:"Tunic top",
+        src:"products/f8.jpg",
+        desc:"",
+        price:1220,
+        tags:["new","gray","summer","Women"]
     },
 
 
